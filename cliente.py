01310180b26d798179b6fcc3078756d11591fbab
Joao Marcos <jmmarmontelo@gmail.com>
2022-07-07 17:12:02 -0300

    adionado comentarios

--- cliente.py
+++ cliente.py
@@ -1,31 +1,49 @@
+
+# Autor Joao Marcos Marmontelo.
+# Trabalho para disciplina Sistemas Distribuidos-2022-01-GCC129.
+# Codigo que represnta o cliente, possui as seguintes funcoes:
+# Primeira conectar ao servidor.
+# Segunda enviar mensagens ao servidor.
+# Terceira receber mensagens do servidor em paraleo com a segunda funcao.
+
 import socket
 import threading
 
-PORT = 3001
-IP = '127.0.0.1'
-nome = input('Digite seu nome: ')
 
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect((IP, PORT))
+def main(): 
+    # Objeto do tipo socket TCP
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def receber():
+    try:
+        # Conectando ao servidor
+        cliente.connect(('127.0.0.1',5555))
+    except:
+        return print('Nao foi possivel conectar ao servidor')
+    
+    nome = input('Digite seu nome: ')
+
+    # Thread que recebe as mensagens do servidor.
+    thread_receber = threading.Thread(target=receberMensagens,args=[cliente]).start()
+    # Thread que envia as mensagens ao servidor.
+    thread_enviar = threading.Thread(target=enviarMensagens, args=[cliente,nome]).start()
+
+# Metodo que recebe as mensagens e printa na tela.
+def receberMensagens(cliente):
     while True:
         try:
-            mensagem = cliente.recv(1024).decode('utf-8')
-            if mensagem == 'NOME':
-                cliente.send(nome.encode('utf-8'))
-            else:
-                print(mensagem)
+            mensagem = cliente.recv(2048).decode('utf-8')
+            print(mensagem +'\n')
         except:
-            print('erro')
+            print('Nao foi possivel estabelecer conexao')
             cliente.close()
             break
-
-def escrever():
+# Metodo que envia as mensagens ao servidor.
+def enviarMensagens(cliente, nome):
     while True:
-        mensagem = f'{nome} : {input()}'
-        cliente.send(mensagem.encode('utf-8'))
+        try:
+            mensagem = input()
+            cliente.send(f'{nome}> {mensagem}'.encode('utf-8'))
+        except:
+            return
 
-receber_thread = threading.Thread(target=receber).start()
-escrever_thread = threading.Thread(target=escrever).start()
-
+main()
--- servidor.py
+++ servidor.py
@@ -1,50 +1,54 @@
+# Autor Joao Marcos Marmontelo
+# Trabalho para disciplina Sistemas Distribuidos-2022-01-GCC129
+# Codigo responssavel pelo servidor, ele faz duas funções em paralelo.
+# Primeira é receber mensagens dos clientes.
+# Segunda enviar transmitir as mensagens para todos os clientes.
 
-#Servidor responsável por receber as mensagens dos clientes
-#e reenvia-las para todos os clientes
 
 import socket
 import threading
 
-# Variaveis ip e porta para a conexao com os clientes
-IP = '127.0.0.1'
-PORT = 3001
-
-servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor.bind((IP, PORT))
-servidor.listen(10)
-
+# Varavel que armazena todos os sockets dos clientes.
 clientes = []
-nomes = []
 
-def transmissor (mensagem):
-    for cliente in clientes:
-        cliente.send(mensagem)
-
-def lidar(cliente):
-    while True:
-        try:
-            mensagem = cliente.recv(1024)
-            transmissor(mensagem)
-        except:
-            index = clientes.index(cliente)
-            clientes.remove(cliente)
-            cliente.close()
-            nome = nomes[index]
-            transmissor(f'{nome} saiu do grupo'.encode('utf-8'))
-            nomes.remove(nome)
-            break
-
-def receber():
+def main():
+    # Objeto do tipo Socket TCP 
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    try:
+        # Metodo bind, representa a interface de concexao, caso necessario basta apenas mudar os parametros.
+        servidor.bind(('127.0.0.1',5555))
+        # Metodo listen, escuta as conexoes, onde o parametro limita a quantiade de conexoes.
+        servidor.listen(20)
+        print('Servidor online')
+    except:
+        return print('Nao foi possivel iniciar o servidor')
+    # While aceita as novas conexoes e abre uma thread para cada cliente e adiciona o cliente ao vetor de clientes.
     while True:
         cliente, endereco = servidor.accept()
-        print(f"Conectou: {str(endereco)}")
-        cliente.send('NOME'.encode('utf-8'))
-        nome = cliente.recv(1024).decode('utf8')
-        nomes.append(nome)
         clientes.append(cliente)
-        transmissor(f'{nome} entrou no grupo'.encode('utf-8'))
-        thread_lidar = threading.Thread(target=lidar, args=(cliente,)).start()
+        threadLidar = threading.Thread(target=lidarMensagens, args=[cliente]).start()
+ 
+# Metodo responsavel por receber as mensagens, enviar para todos os clientes e remover os clientes offline.
+def lidarMensagens(cliente):
+    while True:
+        try:
+            mensagem = cliente.recv(2018)
+            transmissor(mensagem,cliente)
+        except:
+            deletarCliente(cliente)
+            break
 
+# Metodo responsavel por enviar a mensagem ao cliente.
+def transmissor(mensagem, cliente):
+    for clienteI in clientes:
+        try:
+            clienteI.send(mensagem)
+        except:
+            deletarCliente(clienteI)
 
-print('Servidor online')
-receber()
+# Metodo responsavel por remover o cliente da lista de clientes ativos.
+def deletarCliente(cliente):
+    clientes.remove(cliente)
+
+main()
